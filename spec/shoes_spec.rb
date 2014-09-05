80dc9e9d8948514cdedc68766bf9859828a7d26b
Ginger <ginger@luminaryadvisors.com>
2014-09-05 12:44:22 -0700

    prepare specs to join tables

--- spec/shoes_spec.rb
+++ spec/shoes_spec.rb
@@ -12,14 +12,19 @@ end
 
 describe :shoes do
 
-	it 'is initialized with a brand' do
-		shoes = Shoes.new('Converse')
+	it 'is initialized with a brand and shoe ID' do
+		shoes = Shoes.new('Nike', 1)
 		expect(shoes).to be_an_instance_of(Shoes)
 	end
 
 	it 'tells you its brand' do
 		shoes = Shoes.new('Nike')
-		expect(shoes.brand).to eq('Nike')
+		expect(shoes.brand).to eq('Nike', 1)
+	end
+
+	it 'tells you its style ID' do
+		brand = Shoes.new('Nike', 1)
+		expect(brand.style_id). to eq 1
 	end
 
 	it 'starts with no shoes' do
@@ -27,14 +32,14 @@ describe :shoes do
 	end
 
 	it 'lets you save shoes into the database' do
-		shoes = Shoes.new('Adidas')
+		shoes = Shoes.new('Nike', 1)
 		shoes.save
 		expect(Shoes.all).to eq [shoes]
 	end
 
-	it 'is the same shoes if it has the same brand' do
-		shoes1 = Shoes.new('Adidas')
-		shoes2 = Shoes.new('Adidas')
+	it 'is the same shoes if it has the same brand and style ID' do
+		shoes1 = Shoes.new('Nike', 1)
+		shoes2 = Shoes.new('Nike', 1)
 		expect(shoes1).to eq shoes2
 	end
 end
